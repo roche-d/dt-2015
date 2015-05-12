d914f4508043cd988b26651a7da2a6e8dc8e52b5
roche_d <roche_d@epitech.eu>
2015-05-12 17:51:24 +0200

    login

--- mobile-apps/ios/app.js
+++ mobile-apps/ios/app.js
@@ -13,3 +13,40 @@ var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
+
+myApp.onPageInit('view-main', function (page) {
+
+});
+
+myApp.onPageInit('*', function (page) {
+    console.log(page.name + ' initialized');
+});
+
+console.log('ok');
+garagoRun();
+
+function warnInternet() {
+    myApp.addNotification({
+        title: 'Garago',
+        subtitle: 'New message from the Team',
+        message: 'Please remember that this application needs an Internet connection !',
+        media: '<img width="44" height="44" style="border-radius:100%" src="http://lorempixel.com/output/people-q-c-100-100-9.jpg">',
+        onClose: function () {
+            //myApp.alert('Notification closed');
+        },
+        onClick: function () {
+            console.log('click');
+        }
+    });
+};
+
+function garagoRun() {
+    warnInternet();
+
+    var logged = false;
+
+    if (!logged) {
+        console.log('not logged');
+        myApp.loginScreen();
+    }
+};
